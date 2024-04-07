203c085aeab5240d9f9047d244ff79fd6423316b
Kemz <afilfriend@gmail.com>
2024-04-07 20:54:49 +0800

    Add show user data

--- app/User.php
+++ app/User.php
@@ -25,6 +25,14 @@ class User extends Database
 
     public function show($id)
     {
+        $sql    = "SELECT $this->tb_name.*, tb_status.nama as nama_status FROM $this->tb_name JOIN tb_status ON $this->tb_name.status = tb_status.id WHERE $this->tb_name.id = '$id'";
+        $query  = mysqli_query($this->db, $sql);
+
+        $data   = array();
+        while ($temp_user = mysqli_fetch_array($query)) {
+            $data[] = $temp_user;
+        }
+        return $data;
     }
 }
 
